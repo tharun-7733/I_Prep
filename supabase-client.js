a225d94923c79eb046516a6f7deb9c8835efc5dd
tharun-7733 <tharuntej7373@gmail.com>
2026-06-19 16:38:36 +0530

    fix: syntax error

--- supabase-client.js
+++ supabase-client.js
@@ -1,9 +1,7 @@
-/* supabase-client.js */
-
 const SUPABASE_URL = "sb_publishable_BkNTJMFpBmxuLZMr8BGYIA_kUpC_qcR";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5eHVndXhobmFvcWFmcmVrbGJyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MTc5NTg1NSwiZXhwIjoyMDk3MzcxODU1fQ.CRYfs4nyIirPkM11ZPiid76iqNzUCrCBLOOGbFg0e6A";
 
-window.supabaseClient = window.supabase.createClient(
+const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
